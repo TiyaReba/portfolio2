9ffbb95b956f757a1e76050ac7b07508f72a74fa
TiyaReba <105912721+TiyaReba@users.noreply.github.com>
2022-10-19 16:27:02 +0530

    change in ser file

--- src/app/service.service.spec.ts
+++ src/app/service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ServerService } from './server.service';
+import { ServiceService } from './service.service';
 
-describe('ServerService', () => {
-  let service: ServerService;
+describe('ServiceService', () => {
+  let service: ServiceService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ServerService);
+    service = TestBed.inject(ServiceService);
   });
 
   it('should be created', () => {
